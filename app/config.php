f514d9ddf6d47b4e32db1aea85c8372416f0b7fa
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2014-05-14 22:23:34 +0200

    replaced databse configuration

--- app/config.php
+++ app/config.php
@@ -3,6 +3,6 @@
 define('DEFAULT_CONTROLLER','page');
 define('DEFAULT_METHOD','index');
 define('DB_HOST','127.0.0.1');
-define('DB_NAME','database');
+define('DB_NAME','pfe');
 define('DB_USERNAME','root');
-define('DB_PASSWORD','');
+define('DB_PASSWORD','root');
