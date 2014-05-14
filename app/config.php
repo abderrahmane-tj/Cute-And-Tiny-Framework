548bcb4941b695d3f08ce65008e299d553b7f98d
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2014-05-14 22:18:33 +0200

    still playing with config.php

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DEFAULT_CONTROLLER','page');
+define('DEFAULT_METHOD','index');
+define('DB_HOST','127.0.0.1');
+define('DB_NAME','database');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
