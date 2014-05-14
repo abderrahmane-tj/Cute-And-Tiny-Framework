add41bc80010bf9c965362d73b9dcae0b1822571
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2014-05-14 22:25:54 +0200

    finally config is nicely configured

--- app/config.php
+++ app/config.php
@@ -2,7 +2,3 @@
 
 define('DEFAULT_CONTROLLER','page');
 define('DEFAULT_METHOD','index');
-define('DB_HOST','127.0.0.1');
-define('DB_NAME','pfe');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','root');
